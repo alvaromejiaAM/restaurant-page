3bf3342e845d00e3338a57f325a182aa8626b101
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-10-31 16:42:29 -0500

    Add js file for body

---
+++ src/body.js
@@ -0,0 +1,23 @@
+const content = document.querySelector('#content');
+
+  //Create body
+  function createBody(){
+    const body = document.createElement('div');
+    body.classList = 'body';
+    const bodyDesc = document.createElement('p');
+    bodyDesc.innerText = "Welcome to our page, we hope you enjoy our selection of meals! You order and we make sure to add our magic to your next dish!";
+    const menuList = document.createElement('ul');
+    let menuArr = ['New and Popular!', 'Salads & Soups', 'Meal Deals to Go', 'Desserts', 'Gluten Free Options', 'Drinks'];
+    for(let i=0; i<6;i++){
+      const li = document.createElement('li');
+      li.innerText = menuArr[i];
+      body.appendChild(li);
+    }
+    const bodyImage = document.createElement('img');
+    bodyImage.src = ""
+    body.appendChild(bodyDesc);
+    body.appendChild(menuList);
+    content.appendChild(body);
+  }
+
+  export default createBody

8cbac8a8f801092546345661afc7c68f2e449b21
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-10-31 17:03:30 -0500

    combined files into one for single page

---
+++ src/home.js
@@ -0,0 +1,36 @@
+//Create body
+  function home(){
+    const content = document.querySelector('#content');
+  //header
+    const header = document.createElement('div');
+    header.classList = 'header';
+    const headerRestName = document.createElement('h1');
+    headerRestName.innerText = "Babaro's Restaurant";
+    header.appendChild(headerRestName);
+    content.appendChild(header);
+  //body
+    const body = document.createElement('div');
+    body.classList = 'body';
+    const bodyDesc = document.createElement('p');
+    bodyDesc.innerText = "Welcome to our page, we hope you enjoy our selection of meals! You order and we make sure to add our magic to your next dish!";
+    const menuList = document.createElement('ul');
+    let menuArr = ['New and Popular!', 'Salads & Soups', 'Meal Deals to Go', 'Desserts', 'Gluten Free Options', 'Drinks'];
+    for(let i=0; i<6;i++){
+      const li = document.createElement('li');
+      li.innerText = menuArr[i];
+      body.appendChild(li);
+    }
+    const bodyImage = document.createElement('img');
+    bodyImage.src = ""
+    body.appendChild(bodyDesc);
+    body.appendChild(menuList);
+    content.appendChild(body);
+
+   //footer
+    const footer = document.createElement('div');    
+    footer.classList = 'footer';
+    footer.innerText = `Alvaro \u00A92023`
+    content.appendChild(footer);
+  }
+
+  export default home

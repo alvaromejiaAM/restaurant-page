570e07bf5892c2ca0a4b8b2ea53e2ff4bcd9049b
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-11-10 08:20:18 -0600

    update index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import createHome from "./home";
+import createHome from "./pages/home";
+import menu from "./pages/menu";
+
+const menuBtn = document.getElementById('menuTab');
+const homeBtn = document.getElementById('homeTab');
 
 createHome()
+menuTab.addEventListener('click', function(){
+  menu();
+})
+
+homeTab.addEventListener('click', function(){
+  createHome();
+})
+
 
